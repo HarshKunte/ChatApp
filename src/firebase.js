2f3107bd96fbad25530c81f6fe2d6a3069e549e7
HarshKunte <harshkunte99@gmail.com>
2021-05-12 22:55:05 +0530

    init

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,13 @@ import firebase from "firebase";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //GET Below Settings from Firebase > Project Overview > Settings > General > Your apps > Firebase SDK snippet > Config
 const firebaseConfig = {
-    // apiKey: "",
-    // authDomain: "",
-    // databaseURL: "",
-    // projectId: "",
-    // storageBucket: "",
-    // messagingSenderId: "",
-    // appId: "",
-    // measurementId: ""
-    apiKey: "AIzaSyB-TEWDyBk7hMVn4erAU4F_wJVRo3ZFwdI",
-    authDomain: "whatsap-clone-6cd2c.firebaseapp.com",
-    projectId: "whatsap-clone-6cd2c",
-    storageBucket: "whatsap-clone-6cd2c.appspot.com",
-    messagingSenderId: "755030428677",
-    appId: "1:755030428677:web:24c4d629990ab791ede06c",
-    measurementId: "G-L3YEVKH27J"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN ,
+  projectId:  process.env.REACT_APP_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId:  process.env.REACT_APP_MEASUREMENT_ID
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
